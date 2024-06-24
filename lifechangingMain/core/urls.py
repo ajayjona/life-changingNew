04dc0e42e343b1f39327d9ea84e0171f2d07deda
resty <restykyomuhang@gmail.com>
2024-06-25 01:04:10 +0600

    changed index page

---
+++ lifechangingMain/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contributors/', views.contributors, name='contributors'),
+    path('donor/', views.donor, name='donor'),
+]
